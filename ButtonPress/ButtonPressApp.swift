711045a19fbbf79e4530c21ce2c63891b25ee208
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2020-10-21 00:30:26 -0400

    Initial Commit

---
+++ ButtonPress/ButtonPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonPressApp.swift
+//  ButtonPress
+//
+//  Created by Dillan Gajarawala on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct ButtonPressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
